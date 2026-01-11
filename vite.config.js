b03e768a7a3beef82ae41834418b1f1a66dacb4d
yuvanesh366-cmyk <yuvanesh366@gmail.com>
2026-01-11 12:55:53 +0530

    changes

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/basker-portfolio/', // Set this to your repo name
+  base: '/basker_portfolio_1/', // Set this to your repo name
 })
